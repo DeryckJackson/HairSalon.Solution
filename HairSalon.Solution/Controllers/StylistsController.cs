0914510233e02a9d5323e786144cdf7845147584
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-31 10:51:54 -0700

    add edit view on stylists controller

--- HairSalon.Solution/Controllers/StylistsController.cs
+++ HairSalon.Solution/Controllers/StylistsController.cs
@@ -39,5 +39,25 @@ namespace HairSalon.Controllers
       Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
       return View(thisStylist);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+    public ActionResult Delete(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
   }
 }
